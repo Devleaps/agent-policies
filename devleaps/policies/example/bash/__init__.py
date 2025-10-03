5d63118eceaa03cc190fec49f7cb964d61a299c0
Philipp Gayret <philipp@release-engineers.com>
2025-10-03 15:53:46 +0200

    chore: Clean up old examples

--- devleaps/policies/example/bash/__init__.py
+++
@@ -1,3 +0,0 @@
-from .middleware import split_commands_middleware
-
-all_middleware = [split_commands_middleware]
--- devleaps/policies/example/python/__init__.py
+++
@@ -1,3 +0,0 @@
-from .test_file import python_test_file_rule
-
-all_rules = [python_test_file_rule]
